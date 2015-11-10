9061d66545624eb7380713ccaad586325b8253ae
Adam Clark <aclark@e-tcc.com>
2015-11-10 15:01:16 -0500

    added supervisor and mongoose

--- server/app.js
+++ server/app.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var app = express();
 
+var db = require('mongoose');
+db.connect("mongodb://notelyu:notelyp@ds053164.mongolab.com:53164/notelydb");
+
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String,
+  body_text: String,
+  updated_at: { type: Date, default: Date.now }
+});
+
+var Note = db.model('Note', NoteSchema);
+
+
+
 app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
 app.get('/notes', function(req, res){
-  res.json([
-    {
-      title: 'blah note1',
-      body_html: 'this is a hard coded note1.'},
-    {
-      title: 'blah note2',
-      body_html: 'this is a hard coded note2.'
-    }
-  ]);
+  Note.find().then(function(notes){
+    res.json(notes);
+  });
 });
 
 app.listen(3000, function(){
