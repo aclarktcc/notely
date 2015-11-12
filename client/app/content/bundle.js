76a2423ad6158d992a1d7e3b516c53d940887e78
Adam Clark <aclark@e-tcc.com>
2015-11-12 11:03:04 -0500

    added signup form

--- client/app/content/bundle.js
+++ client/app/content/bundle.js
@@ -14,60 +14,11 @@
 })();
 'use strict';
 
-(function () {
-
-  angular.module('notely.notes', ['ui.router', 'textAngular']).config(notesConfig);
-
-  notesConfig['$inject'] = ['$stateProvider'];
-  function notesConfig($stateProvider) {
-    $stateProvider.state('notes', {
-      url: '/notes',
-      resolve: {
-        notesLoaded: ['NotesService', function (NotesService) {
-          return NotesService.fetch();
-        }]
-      },
-      templateUrl: '/notes/notes.html',
-      controller: NotesController
-    }).state('notes.form', {
-      url: '/:noteId',
-      templateUrl: '/notes/notes-form.html',
-      controller: NotesFormController
-    });
-  }
-
-  NotesController.$inject = ['$state', '$scope', 'NotesService'];
-  function NotesController($state, $scope, NotesService) {
-    $scope.note = {};
-    $scope.notes = NotesService.get();
-  }
-
-  NotesFormController.$inject = ['$scope', '$state', 'NotesService'];
-  function NotesFormController($scope, $state, NotesService) {
-    $scope.note = NotesService.findById($state.params.noteId);
-
-    $scope.save = function () {
-      // Decide whether to call create or update...
-      if ($scope.note._id) {
-        NotesService.update($scope.note).then(function (response) {
-          $scope.note = angular.copy(response.data.note);
-        });
-      } else {
-        NotesService.create($scope.note).then(function (response) {
-          $state.go('notes.form', { noteId: response.data.note._id });
-        });
-      }
-    };
-    //delete
-    $scope['delete'] = function () {
-      NotesService['delete']($scope.note).then(function () {
-        $state.go('notes.form', { noteId: undefined });
-      });
-    };
-  }
-})();
-
-//
+angular.module('notely').directive('signUp', function () {
+  return {
+    templateUrl: '/components/sign-up.html'
+  };
+});
 'use strict';
 
 angular.module('notely').service('NotesService', NotesService);
@@ -156,4 +107,74 @@ function NotesService($http, API_BASE) {
 }
 
 //
+'use strict';
+
+(function () {
+
+  angular.module('notely.notes', ['ui.router', 'textAngular']).config(notesConfig);
+
+  notesConfig['$inject'] = ['$stateProvider'];
+  function notesConfig($stateProvider) {
+    $stateProvider.state('notes', {
+      url: '/notes',
+      resolve: {
+        notesLoaded: ['NotesService', function (NotesService) {
+          return NotesService.fetch();
+        }]
+      },
+      templateUrl: '/notes/notes.html',
+      controller: NotesController
+    }).state('notes.form', {
+      url: '/:noteId',
+      templateUrl: '/notes/notes-form.html',
+      controller: NotesFormController
+    });
+  }
+
+  NotesController.$inject = ['$state', '$scope', 'NotesService'];
+  function NotesController($state, $scope, NotesService) {
+    $scope.note = {};
+    $scope.notes = NotesService.get();
+  }
+
+  NotesFormController.$inject = ['$scope', '$state', 'NotesService'];
+  function NotesFormController($scope, $state, NotesService) {
+    $scope.note = NotesService.findById($state.params.noteId);
+
+    $scope.save = function () {
+      // Decide whether to call create or update...
+      if ($scope.note._id) {
+        NotesService.update($scope.note).then(function (response) {
+          $scope.note = angular.copy(response.data.note);
+        });
+      } else {
+        NotesService.create($scope.note).then(function (response) {
+          $state.go('notes.form', { noteId: response.data.note._id });
+        });
+      }
+    };
+    //delete
+    $scope['delete'] = function () {
+      NotesService['delete']($scope.note).then(function () {
+        $state.go('notes.form', { noteId: undefined });
+      });
+    };
+  }
+})();
+
+//
+'use strict';
+
+(function () {
+  angular.module('notely').config(usersConfig);
+
+  usersConfig.$inject = ['$stateProvider'];
+
+  function usersConfig($stateProvider) {
+    $stateProvider.state('sign-up', {
+      url: '/sign-up',
+      template: '<sign-up></sign-up>'
+    });
+  };
+})();
 //# sourceMappingURL=bundle.js.map
